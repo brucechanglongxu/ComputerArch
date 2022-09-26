a69f6835a8b0615f147f5eccf83f39f2afb60f69
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-26 10:10:19 -0700

    Update blocking_vs_nonblocking.v

--- blocking_vs_nonblocking.v
+++ blocking_vs_nonblocking.v
@@ -1,1 +1,11 @@
-# 
+// Demonstrating the difference between blocking and non-blocking assignments
+  logic clk, we;
+  logic [1:0] a;
+  logic [3:0] di, do;
+  logic [7:0] mem [3:0];
+  
+// The standard equals sign is the normal blocking assignment
+  always_ff @(posedge clk) begin
+    if(we) mem[a] = di;
+    do = mem[a];
+  end
