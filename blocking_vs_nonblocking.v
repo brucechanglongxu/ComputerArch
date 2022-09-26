75422a00803d8babff68f32bb654fe8d344274de
Bruce Xu <brucechanglongxu@gmail.com>
2022-09-26 10:16:05 -0700

    Update blocking_vs_nonblocking.v

--- blocking_vs_nonblocking.v
+++ blocking_vs_nonblocking.v
@@ -5,6 +5,7 @@
   logic [7:0] mem [3:0];
   
 // The standard equals sign is the normal blocking assignment
+// Our lines of code appear to happen in sequence. 
   always_ff @(posedge clk) begin
     if(we) mem[a] = di;
     do = mem[a];
